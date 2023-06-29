2259982f1bed41db774fc37c5e9f750e3b945f3d
John Mark <johomurk@gmail.com>
2023-06-29 11:23:17 +0000

    Replace .gitignore in the correct directory

--- api/index.js
+++ api/index.js
@@ -4,24 +4,21 @@ const { Configuration, OpenAIApi } = require("openai");
 const OPENAI_API_KEY = process.env.OPENAI_SWISSLATOR_KEY;
 
 // Set up OpenAI API client
-const configuration = new Configuration({apiKey: OPENAI_API_KEY,});
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(new Configuration({apiKey: OPENAI_API_KEY,}));
 
 // Function to translate Swiss German text
 async function translateSwissGerman(text) {
-  const prompt = `Translate the following Swiss German text to English:\n\n${text}`;
+  const prompt = `Translate the following Swiss German text to English: ${text}`;
   try {
     // Call the OpenAI API to generate translation
     const response = await openai.createCompletion({
-      model: 'gpt-3.5-turbo',
+      model: 'text-davinci-003',
       prompt: prompt,
-      maxTokens: 100,
-      temperature: 0.5,
-      topP: 1.0,
-      n: 1,
-      stop: '\n'
+      max_tokens: 400,
+      temperature: 0.3,
+      top_p: 1.0,
+      n: 1
     });
-
     // Extract and return the translated text
     const translation = response.data.choices[0].text.trim();
     return translation;
@@ -31,13 +28,18 @@ async function translateSwissGerman(text) {
   }
 }
 
-// Example usage
-const swissGermanText = 'Grüezi, wie gohts?';
-translateSwissGerman(swissGermanText)
-  .then(translation => {
-    console.log(`Swiss German: ${swissGermanText}`);
-    console.log(`Translation: ${translation}`);
-  })
-  .catch(error => {
-    console.error('Translation failed:', error);
-  });
+// // Example usage
+// const swissGermanText = `Hüt isch d Thermik ächli besser gsy aus geschter, Nord- und Oschtüberdruck isch immer no dütlich prognoschtiziert gsy. Ohni Christoph wäri äuä nid überd Grimsel, und ohni Stephi und ihri Tips vom Morgä wäri äuä au nid id Südtäler. Beidi Entscheidigä hani nid bereut...
+
+// Merci vöu vöu Mou Raoul für Choscht und Logis.
+
+// Sion aktiv bis 17:05 Uhr. Am frühen Nachmittag 300m AGL unterflogen.`;
+
+// translateSwissGerman(swissGermanText)
+//   .then(translation => {
+//     console.log(`Swiss German: ${swissGermanText}`, "\n");
+//     console.log(`Translation: ${translation}`);
+//   })
+//   .catch(error => {
+//     console.error('Translation failed:', error);
+//   });
